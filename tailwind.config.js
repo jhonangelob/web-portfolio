6c5a3d4cbf9d13dea8755c35b575b71551f9d678
jhonangeloB <bustarde.aj@gmail.com>
2022-11-14 22:01:48 +0800

    Add tailwind css color themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
   theme: {
+    colors: {
+      'primary-color': '#303030',
+      'secondary-color': '#ffa000',
+      'black-color': '#060606',
+      'lightgray-color': '#e4e4e4',
+      'gray-color': '#657688',
+      'brown-color': '#46364a',
+      'blue-color': '#3e3b4c',
+    },
     extend: {},
   },
   plugins: [],
