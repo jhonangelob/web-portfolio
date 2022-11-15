8e44d7004f2d57bfbb2cf2bcb32e9572c82931d0
jhonangeloB <bustarde.aj@gmail.com>
2022-11-16 00:45:22 +0800

    Add hero components element and stylings

--- pages/components/Hero.jsx
+++ pages/components/Hero.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <div className='flex justify-center items-center p-5 h-screen'>
-      <h1>Jhon Angelo Bustarde</h1>
+    <div className='flex items-end p-5 max-w-6xl m-auto h-screen'>
+      {/* <h1 className='font-extrabold text-6xl text-accent-color  max-w-7xl md:text-9xl'>
+        jhn.
+      </h1> */}
+      <h1 className='text-primary-color font-semibold mb-40 text-5xl max-w-4xl md:text-6xl dark:text-white-color'>
+        Hello, my name is Jhon Angelo Bustarde and I am a Web Developer.
+      </h1>
+
+      {/* <div className='mb-40'>
+        <button className='bg-accent-color'>Let&apos;s Talk</button>
+        <button className='bg-bg-color'>Let&apos;s Talk</button>
+      </div> */}
     </div>
   );
 };
