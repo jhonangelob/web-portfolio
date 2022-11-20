f7a500d02ebe374c7d76376e5f947c5e7c066881
jhonangeloB <bustarde.aj@gmail.com>
2022-11-21 00:01:45 +0800

    Add delay on animation

--- pages/components/About.jsx
+++ pages/components/About.jsx
@@ -7,22 +7,26 @@ const About = () => {
       <h1 className='text-primary-color text-5xl leading-normal font-bold md:text-8xl dark:text-secondary-color'>
         I specializes in{' '}
         <motion.span
+          className='text-secondary-color'
           whileInView={{
             color: '#ff0049',
           }}
           transition={{
-            duration: 0.75,
+            duration: 0.5,
+            delay: 0.25,
           }}
         >
           Front-End
         </motion.span>{' '}
         Web Development and{' '}
         <motion.span
+          className='text-secondary-color'
           whileInView={{
             color: '#ff0049',
           }}
           transition={{
             duration: 0.5,
+            delay: 0.5,
           }}
         >
           UI/UX
