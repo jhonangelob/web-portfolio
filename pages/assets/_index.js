041e73d10fae94ea0cbdd7c2ce5599ec169d1182
jhonangeloB <bustarde.aj@gmail.com>
2022-11-20 19:04:45 +0800

    Add svg imports indexing

---
+++ pages/assets/_index.js
@@ -0,0 +1,29 @@
+import contentful from './contentful.svg';
+import css from './css.svg';
+import figma from './figma.svg';
+import firebase from './firebase.svg';
+import git from './git.svg';
+import html from './html.svg';
+import javascript from './javascript.svg';
+import nextjs from './nextjs.svg';
+import nodejs from './nodejs.svg';
+import npm from './npm.svg';
+import reactjs from './reactjs.svg';
+import sass from './sass.svg';
+import tailwindcss from './tailwindcss.svg';
+
+export {
+  contentful,
+  css,
+  figma,
+  firebase,
+  git,
+  html,
+  javascript,
+  nextjs,
+  nodejs,
+  npm,
+  reactjs,
+  sass,
+  tailwindcss,
+};
