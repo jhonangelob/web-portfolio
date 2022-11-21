d9d272683e26dda48346cefbc7099756c43305d8
jhonangeloB <bustarde.aj@gmail.com>
2022-11-21 17:51:08 +0800

    Fix styling

--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='flex bg-bg-color justify-center p-4'>
-      <span className='text-secondary-color font-thin text-sm'>
+    <footer className='flex  bg-bg-color justify-center p-4 w-screen'>
+      <span className='text-secondary-color font-thin text-sm text-center'>
         © 2022 Jhon Angelo Bustarde
       </span>
     </footer>
