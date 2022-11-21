78794fdbd52a507cca4b8032ba06d2e5b4dd51a6
jhonangeloB <bustarde.aj@gmail.com>
2022-11-21 16:40:45 +0800

    Add Experience component transition/presence animation

--- pages/components/Experience.jsx
+++ pages/components/Experience.jsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import React from 'react';
+import { motion } from 'framer-motion';
+
 import {
   contentful,
   css,
@@ -25,25 +27,60 @@ function Experience() {
     <div className='flex flex-col justify-center max-w-6xl gap-10 m-auto p-5 min-h-screen'>
       <h1 className='text-5xl font-bold text-secondary-color md:max-w-2xl md:text-7xl'>
         My Web Development{' '}
-        <span className='text-white-color'>Technologies</span> and{' '}
-        <span className='text-gray-color'>Experience</span>.
+        <motion.span
+          whileInView={{
+            opacity: [0, 1],
+            color: ['#c4c4c4c4', '#ffffff'],
+          }}
+          transition={{
+            duration: 1,
+          }}
+        >
+          Technologies
+        </motion.span>{' '}
+        and{' '}
+        <motion.span
+          whileInView={{
+            opacity: [0, 1],
+            color: ['#696969', '#c4c4c4'],
+          }}
+          transition={{
+            duration: 1,
+            delay: 0.5,
+          }}
+        >
+          Experience
+        </motion.span>
+        .
       </h1>
       <div className='flex flex-col w-full gap-5 md:flex-row text-secondary-color'>
-        <div className='md:w-3/6 flex flex-col gap-y-5'>
+        <motion.div
+          className='md:w-3/6 flex flex-col gap-y-5'
+          whileInView={{
+            opacity: [0, 1],
+            color: ['#696969', '#c4c4c4'],
+          }}
+          transition={{
+            duration: 1,
+          }}
+        >
           <h3 className='text-accent-color'>Technologies</h3>
           <p className='italic'>
             The technologies I use for front-end development
           </p>
           <div className='flex gap-5 flex-wrap'>
             {frontend?.map((item, index) => (
-              <Image
+              <div
                 key={index}
-                className='md:grayscale md:hover:grayscale-0'
-                src={item}
-                alt={item}
-                width={40}
-                height={40}
-              />
+                style={{ width: '40px', height: '40px', position: 'relative' }}
+              >
+                <Image
+                  className='md:grayscale md:hover:grayscale-0'
+                  src={item}
+                  alt={item}
+                  layout='fill'
+                />
+              </div>
             ))}
           </div>
           <p className='italic'>
@@ -51,36 +88,55 @@ function Experience() {
           </p>
           <div className='flex gap-5'>
             {backend?.map((item, index) => (
-              <Image
+              <div
                 key={index}
-                className='md:grayscale md:hover:grayscale-0'
-                src={item}
-                alt={item}
-                width={40}
-                height={40}
-              />
+                style={{ width: '40px', height: '40px', position: 'relative' }}
+              >
+                <Image
+                  key={index}
+                  className='md:grayscale md:hover:grayscale-0'
+                  src={item}
+                  alt={item}
+                  layout='fill'
+                />
+              </div>
             ))}
           </div>
           <p className='italic'>others...</p>
           <div className='flex gap-5'>
             {others?.map((item, index) => (
-              <Image
+              <div
                 key={index}
-                className='md:grayscale md:hover:grayscale-0'
-                src={item}
-                alt={item}
-                width={40}
-                height={40}
-              />
+                style={{ width: '40px', height: '40px', position: 'relative' }}
+              >
+                <Image
+                  key={index}
+                  className='md:grayscale md:hover:grayscale-0'
+                  src={item}
+                  alt={item}
+                  width={40}
+                  height={40}
+                />
+              </div>
             ))}
           </div>
-        </div>
-        <div className='md:w-3/6 flex flex-col gap-2'>
+        </motion.div>
+        <motion.div
+          className='md:w-3/6 flex flex-col gap-2'
+          whileInView={{
+            opacity: [0, 1],
+            color: ['#696969', '#c4c4c4'],
+          }}
+          transition={{
+            duration: 1,
+            delay: 0.5,
+          }}
+        >
           <h3 className='text-accent-color'>Experience/s</h3>
           <p>2022</p>
           <h3 className='text-white-color font-medium'>Web Developer Intern</h3>
           <p>Symph, Inc.</p>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
