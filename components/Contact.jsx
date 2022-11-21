bef5733852a7f9ff55a579855d93c0ac1487b223
jhonangeloB <bustarde.aj@gmail.com>
2022-11-21 23:41:37 +0800

    Fix scaling and remove back to top button

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { FaChevronUp } from 'react-icons/fa';
 import { SocialIcon } from 'react-social-icons';
 
 const links = [
@@ -9,13 +8,9 @@ const links = [
 ];
 
 const Contact = () => {
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0 });
-  };
-
   return (
     <div
-      className='flex flex-col max-w-4xl justify-center min-h-screen p-5 m-auto'
+      className='flex flex-col max-w-4xl justify-center min-h-screen p-5 m-auto md:mt-0'
       id='contact'
     >
       <h1 className='font-bold text-6xl mb-10 text-center text-black-color md:text-9xl'>
@@ -29,7 +24,7 @@ const Contact = () => {
           <h3 className='font-semibold mb-4'>
             Leave me a <span className='text-accent-color'>message</span>
           </h3>
-          <form action='' className='flex flex-col gap-4 text-base font-light'>
+          <form action='' className='flex flex-col gap-2 text-base font-light'>
             <input
               type='text'
               name='email'
@@ -51,18 +46,17 @@ const Contact = () => {
             </button>
           </form>
         </div>
-        <div className='w-100 gap-5 flex flex-col text-gray-color items-center md:w-2/6 md:items-start md:pl-4'>
-          <h1 className='font-semibold text-3xl text-white-color'>
+        <div className='w-100 gap-2 flex flex-col text-center md:text-left text-gray-color items-center md:w-2/6 md:items-start md:pl-4'>
+          <h1 className='font-semibold text-3xl text-white-color '>
             Let&apos;s talk about anything.
           </h1>
-          <div className='flex flex-col text-sm gap-2'>
+          <div className='flex flex-col text-sm gap-2 '>
             <p>You can learn more about me on my Resume.</p>
-            <button className='text-accent-color font-semibold text-center md:text-left'>
+            <button className='text-accent-color text-lg font-semibold text-center md:text-left'>
               Grab a copy
             </button>
           </div>
           <div className='flex flex-col text-sm gap-1 items-center md:items-start'>
-            <h3 className='font-bold'>Stay Connected</h3>
             <span>Calamba, Laguna</span>
             <span>bustarde.aj@gmail.com</span>
           </div>
@@ -77,21 +71,6 @@ const Contact = () => {
                 style={{ width: 35, height: 35 }}
               />
             ))}
-          </div>
-          <div
-            className='flex flex-col gap items-center cursor-pointer'
-            onClick={scrollToTop}
-          >
-            <FaChevronUp
-              className='flex md:hidden fill-white-color'
-              style={{
-                width: 15,
-                height: 15,
-              }}
-            />
-            <span className='font-semibold text-white-color text-sm text-center'>
-              BACK TO TOP
-            </span>
           </div>
         </div>
       </div>
