bc92e49b42a21b9ca2895f403a43ae54e19f6ec6
jhonangeloB <bustarde.aj@gmail.com>
2022-12-06 01:48:32 +0800

    Fix alignment

--- components/About.jsx
+++ components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className='flex flex-co justify-center p-5 max-w-6xl m-auto h-screen'>
+    <div className='flex flex-co justify-center items-center p-5 max-w-6xl m-auto h-screen'>
       <h1 className='text-primary-color text-5xl leading-normal font-bold md:text-8xl dark:text-secondary-color'>
         I specialize in{' '}
         <motion.span
