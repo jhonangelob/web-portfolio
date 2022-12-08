0090fe491bef34a9567a64dc5fb9a2678e512b3d
jhonangeloB <bustarde.aj@gmail.com>
2022-12-08 14:32:18 +0800

    Update theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,13 @@ module.exports = {
   darkMode: 'class',
   theme: {
     colors: {
-      'white-color': '#fff',
-      'bg-color': '#191919',
       'primary-color': '#303030',
       'secondary-color': '#696969',
       'accent-color': '#ff0049',
-      'black-color': '#060606',
+      'dark-color': '#000',
+      'light-color': '#fff',
       'gray-color': '#c4c4c4',
+      'background-color': '#191919',
     },
     extend: {
       fontFamily: {
