64fcf50f963912f8f064669e2a845c2b21e4b246
jhonangeloB <bustarde.aj@gmail.com>
2022-12-09 20:28:24 +0800

    Add emailjs function

---
+++ configs/sendEmail.js
@@ -0,0 +1,13 @@
+export const sendEmail = async (form) => {
+  try {
+    const data = await emailjs.sendForm(
+      process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+      process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+      form,
+      process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
+    );
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
